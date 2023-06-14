c2081d9e2fcc155fbd5fe386895a4c9e80c22f87
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-06-14 17:20:04 -0400

    👌 IMPROVE: spelling error

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.scss'
 
 function App() {
 
-  const [corrdinatesA, setCorrdinatesA] = useState({lat:0, long:0});
-  const [corrdinatesB, setCorrdinatesB] = useState({lat:0, long:0});
+  const [coordinatesA, setCoordinatesA] = useState({lat:0, long:0});
+  const [coordinatesB, setCoordinatesB] = useState({lat:0, long:0});
   const [distance, setDistance] = useState(0);
 
   const LatitudeDegreeConvertions = {toKilometers: 40075.017/360, toMile: 24901.461/360}
@@ -13,17 +13,17 @@ function App() {
   function handleSubmit(event){
     event.preventDefault()
 
-    let latitudeDifference = (corrdinatesA.lat > corrdinatesB.lat ? corrdinatesA.lat - corrdinatesB.lat : corrdinatesB.lat - corrdinatesA.lat) * LatitudeDegreeConvertions.toKilometers
-    let longitudeDifference = (corrdinatesA.long > corrdinatesB.long ? corrdinatesA.long - corrdinatesB.long : corrdinatesB.long - corrdinatesA.long) * LongitudeDegreeConvertions.toKilometers
+    let latitudeDifference = (coordinatesA.lat > coordinatesB.lat ? coordinatesA.lat - coordinatesB.lat : coordinatesB.lat - coordinatesA.lat) * LatitudeDegreeConvertions.toKilometers
+    let longitudeDifference = (coordinatesA.long > coordinatesB.long ? coordinatesA.long - coordinatesB.long : coordinatesB.long - coordinatesA.long) * LongitudeDegreeConvertions.toKilometers
 
     setDistance(Math.sqrt(Math.pow(latitudeDifference,2) + Math.pow(longitudeDifference,2)) + ' km')
   }
 
   function handlePointChange(point, update){
     if(point == 'A'){
-      setCorrdinatesA(corrdinatesA => ({...corrdinatesA, ...update}))
+      setCoordinatesA(coordinatesA => ({...coordinatesA, ...update}))
     } else {
-      setCorrdinatesB(corrdinatesB => ({...corrdinatesB, ...update}))
+      setCoordinatesB(coordinatesB => ({...coordinatesB, ...update}))
     }
   }
 
@@ -35,24 +35,24 @@ function App() {
 
         <div className='form-group'>
           <lable>Latitutde</lable>
-          <input type='number' value={corrdinatesA.lat} onChange={e => handlePointChange('A',{lat:parseFloat(e.target.value)})} />
+          <input type='number' value={coordinatesA.lat} onChange={e => handlePointChange('A',{lat:parseFloat(e.target.value)})} />
         </div>
 
         <div className='form-group'>
           <lable>Longitude</lable>
-          <input type='number' value={corrdinatesA.long} onChange={e => handlePointChange('A',{long:parseFloat(e.target.value)})} />
+          <input type='number' value={coordinatesA.long} onChange={e => handlePointChange('A',{long:parseFloat(e.target.value)})} />
         </div>
 
         <h2>Point B</h2>
 
         <div className='form-group'>
           <lable>Latitutde</lable>
-          <input type='number' value={corrdinatesB.lat} onChange={e => handlePointChange('B',{lat:parseFloat(e.target.value)})} />
+          <input type='number' value={coordinatesB.lat} onChange={e => handlePointChange('B',{lat:parseFloat(e.target.value)})} />
         </div>
 
         <div className='form-group'>
           <lable>Longitude</lable>
-          <input type='number' value={corrdinatesB.long} onChange={e => handlePointChange('B',{long:parseFloat(e.target.value)})} />
+          <input type='number' value={coordinatesB.long} onChange={e => handlePointChange('B',{long:parseFloat(e.target.value)})} />
         </div>
 
         <input type='submit' />
