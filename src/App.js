f473eba3131ade7dc6ec99c8e56ab60d4cb917ad
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-06-15 20:22:24 -0400

    📦 NEW: map and calculations work, and all features are present

--- src/App.js
+++ src/App.js
@@ -1,64 +1,82 @@
-import React, { useState } from 'react';
-import './App.scss'
+import React, { useState, useEffect, useCallback, useDeferredValue } from "react";
+import { SearchBox, AddressMinimap, useConfirmAddress, config } from "@mapbox/search-js-react";
+import "./App.scss"
 
 function App() {
 
-  const [coordinatesA, setCoordinatesA] = useState({lat:0, long:0});
-  const [coordinatesB, setCoordinatesB] = useState({lat:0, long:0});
+  const [feature, setFeature] = useState({properties:{full_address:""}, geometry:{coordinates:[-84.3877, 33.7488]}});
+  const [searchValue, setSearchValue] = useState("");
+  const [pointAValue, setPointAValue] = useState("");
+  const [pointBValue, setPointBValue] = useState("");
+  const [token, setToken] = useState("");
   const [distance, setDistance] = useState(0);
 
   const LatitudeDegreeConvertions = {toKilometers: 40075.017/360, toMile: 24901.461/360}
   const LongitudeDegreeConvertions = {toKilometers: 40007.863/360, toMile: 24859.734/360}
 
-  function handleSubmit(event){
-    event.preventDefault()
+  useEffect(() => {
+    const accessToken = "pk.eyJ1IjoiYy1yb21hbmNhbnRyZWxsIiwiYSI6ImNsZ3h6MmliOTA0Z3IzZXBpZWpqOWNtbzQifQ.AA1tCkKxeoIpw4YGUsYTJQ"
+    setToken(accessToken)
+    config.accessToken = accessToken;
+  }, []);
 
-    let latitudeDifference = (coordinatesA.lat > coordinatesB.lat ? coordinatesA.lat - coordinatesB.lat : coordinatesB.lat - coordinatesA.lat) * LatitudeDegreeConvertions.toKilometers
-    let longitudeDifference = (coordinatesA.long > coordinatesB.long ? coordinatesA.long - coordinatesB.long : coordinatesB.long - coordinatesA.long) * LongitudeDegreeConvertions.toKilometers
+  const handleRetrieve = useCallback(
+    (res) => {
+      const feature = res.features[0]
+      setFeature(feature)
+    },[setFeature])
 
-    setDistance(Math.sqrt(Math.pow(latitudeDifference,2) + Math.pow(longitudeDifference,2)) + ' km')
+  function handleSubmit(e){
+    e.preventDefault()
+    let pointA = stringToCoordinates(pointAValue)
+    let pointB = stringToCoordinates(pointBValue)
+    setDistance(calculateDistance(pointA, pointB))
   }
 
-  function handlePointChange(point, update){
-    if(point == 'A'){
-      setCoordinatesA(coordinatesA => ({...coordinatesA, ...update}))
-    } else {
-      setCoordinatesB(coordinatesB => ({...coordinatesB, ...update}))
-    }
+  function stringToCoordinates(stringCoordinates){
+    stringCoordinates = stringCoordinates.split(',').map(item => item.trim())
+    return {long:parseFloat(stringCoordinates[0]), lat: parseFloat(stringCoordinates[1])}
+  }
+
+  function calculateDistance(firstPoint, secondPoint){
+    let xAxisDifference = (firstPoint.long <= secondPoint.long ? secondPoint.long - firstPoint.long : firstPoint.long - secondPoint.long) * LongitudeDegreeConvertions.toKilometers
+    let yAxisDifference = (firstPoint.lat <= secondPoint.lat ? secondPoint.lat - firstPoint.lat : firstPoint.lat - secondPoint.lat) * LatitudeDegreeConvertions.toKilometers
+    return Math.sqrt(Math.pow(xAxisDifference, 2) + Math.pow(yAxisDifference, 2)).toFixed(3)
   }
 
   return (
     <>
-      <form onSubmit={e => handleSubmit(e)}>
+      <div>
 
+        <div>
+        <h2 className="txt-s txt-bold color-gray mb3">Address</h2>
+        <SearchBox accessToken={token} onRetrieve={handleRetrieve} value={searchValue} onChange={value=>setSearchValue(value)} />
+        </div>
+
+        <div className="test">
+          <AddressMinimap satelliteToggle={true} feature={feature} show={true}/>
+        </div>
+
+        <button onClick={event=>setPointAValue(`${feature.geometry.coordinates[0]}, ${feature.geometry.coordinates[1]}`)}>SET POINT A</button>
+        <button onClick={event=>setPointBValue(`${feature.geometry.coordinates[0]}, ${feature.geometry.coordinates[1]}`)}>SET POINT B</button>
+
+        <p>Longitude: {feature.geometry.coordinates[0]}</p>
+        <p>Latitude: {feature.geometry.coordinates[1]}</p>
+      </div>
+
+      <form onSubmit={event=>handleSubmit(event)}>
         <h2>Point A</h2>
+        <input type="text" value={pointAValue} onChange={setPointAValue}/>
 
-        <div className='form-group'>
-          <lable>Latitutde</lable>
-          <input type='number' value={coordinatesA.lat} onChange={e => handlePointChange('A',{lat:parseFloat(e.target.value)})} />
-        </div>
+        <h2>point B</h2>
+        <input type="text" value={pointBValue} onChange={setPointBValue} />
 
-        <div className='form-group'>
-          <lable>Longitude</lable>
-          <input type='number' value={coordinatesA.long} onChange={e => handlePointChange('A',{long:parseFloat(e.target.value)})} />
-        </div>
-
-        <h2>Point B</h2>
-
-        <div className='form-group'>
-          <lable>Latitutde</lable>
-          <input type='number' value={coordinatesB.lat} onChange={e => handlePointChange('B',{lat:parseFloat(e.target.value)})} />
-        </div>
-
-        <div className='form-group'>
-          <lable>Longitude</lable>
-          <input type='number' value={coordinatesB.long} onChange={e => handlePointChange('B',{long:parseFloat(e.target.value)})} />
-        </div>
-
-        <input type='submit' />
-
+        <input type="submit" />
       </form>
-      <h2>{distance}</h2>
+
+      <div>
+        <h2>Distance: {distance}km</h2>
+      </div>
     </>
   );
 }
